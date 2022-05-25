6e90ea3928cddc4684eea64e80afcd87f3858262
Bodea Teofil <teo.bodea12@yahoo.com>
2022-05-25 23:55:34 +0200

    finish basic window class

--- Sprytr-core/src/graphics/window.cpp
+++ Sprytr-core/src/graphics/window.cpp
@@ -7,28 +7,19 @@ namespace sprytr {
 
         Window::Window(const char* title, int width, int height)
         {
+            //TODO: See if I want to throw exception
             m_title = title;
             init(width, height);
         }
 
         Window::~Window() 
         {
-            //TODO: See if I want to terminate here or in spytr function
-            glfwTerminate();
+            
         }
 
         void Window::init(int width, int height)
         {
-            if (!glfwInit())
-            {
-                std::cout << "Failed to initialize GLFW." << std::endl;
-                exit(EXIT_FAILURE);
-            }
-
-            //configure options
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+           
 
             m_window = glfwCreateWindow(width, height, m_title, NULL, NULL);
 
@@ -41,9 +32,8 @@ namespace sprytr {
             glfwMakeContextCurrent(m_window);
             glfwSetFramebufferSizeCallback(m_window, framebuffer_size_callback);
 
-            //TODO: See if I want to do this here or in a separate sprytr init function
             //load openGL function pointers
-            if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+            if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
             {
                 std::cout << "Failed to initialize GLAD" << std::endl;
                 exit(EXIT_FAILURE);
@@ -64,13 +54,28 @@ namespace sprytr {
 
         void Window::update() const
         {
+            make_current();
             glfwSwapBuffers(m_window);
-            glfwPollEvents();
         }
 
         void Window::get_size(int& width, int& height) const
         {
             glfwGetFramebufferSize(m_window, &width, &height);
+        }
+
+        void Window::make_current() const
+        {
+            glfwMakeContextCurrent(m_window);
+        }
+
+        void Window::set_clear_color(float red, float green, float blue, float alpha) const
+        {
+            glClearColor(red, green, blue, alpha);
+        }
+
+        void Window::clear() const
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
         }
     }
 }
--- Sprytr-core/src/graphics/window.h
+++ Sprytr-core/src/graphics/window.h
@@ -3,21 +3,30 @@
 #include <GLFW/glfw3.h>
 
 namespace sprytr {
+
+    class Sprytr;
     namespace graphics {
-     
+        
         class Window {
+            
         private: 
             const char* m_title;
             GLFWwindow* m_window;
+            
         private:
             void init(int width, int height);
             static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+            friend class sprytr::Sprytr;
+           
         public:
             Window(const char* title, int width, int height);
             ~Window();
             bool should_close() const;
             void update() const;
             void get_size(int& width, int& height) const;
+            void make_current() const;
+            void set_clear_color(float x, float y, float z, float a) const;
+            void clear() const;
         };
     }
 }
